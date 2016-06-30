d192c7197a0e20c8302094da90fca92d9656f368
Kirill74 <vtnhjy2832@gmail.com>
2016-06-30 17:58:06 +0300

    gallery

--- src/js/popap.js
+++ src/js/popap.js
@@ -10,3 +10,11 @@ $('.popup_exit').on('click', function(event){
   $('body').removeClass('ovh')
   $('.popup_around').removeClass('popup_active') 
 });
+$('.tabs__form-leftside').on('click', function(event){
+  event.preventDefault();
+  $('.tabs__form').addClass('popup_active') 
+});
+$('.tabs__form-exit').on('click', function(event){
+  event.preventDefault();
+  $('.tabs__form').removeClass('popup_active') 
+});
